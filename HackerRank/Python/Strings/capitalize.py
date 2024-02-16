d38675003e1d452912d364c3b856a00d473247b6
Anirudh Gajjala <anirudh.gajjala2000@gmail.com>
2024-02-16 16:10:53 -0500

    Day 4
    - Strings (Easy)
    
    	- Text Wrap
    	- String Formatting
    	- Capitalize!

---
+++ HackerRank/Python/Strings/capitalize.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+
+def solve(s):
+    name = s.split(' ')
+    for i in range(len(name)):
+        name[i] = name[i].capitalize()
+    return ' '.join(name)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
